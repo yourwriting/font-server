e3c4b2951c59ddc748adca34290172aa7713dd89
leejin_rho <tbdpapdl@gmail.com>
2023-10-26 17:02:28 +0900

    chore: jamo3 위치, 크기 수정

--- jamo3.py
+++ jamo3.py
@@ -45,8 +45,8 @@ def combine_letters3(input_folder, output_folder):
     consonants_1 = [(i, load_image(os.path.join(input_folder, f'{i}.PNG'))) for i in consonants_index]
     consonants = [(c_value, rescale_image_ratio(img, rescale_width=47)) for
                                         c_value, img in consonants_1]
-    consonants_2 = [(c_value, rescale_image_ratio(img, rescale_width=55)) for
-                                        c_value, img in consonants_1]
+    consonants_2 = [(c2_value, rescale_image_ratio(img, rescale_width=55)) for
+                                        c2_value, img in consonants_1]
     #ㅏ, ㅐ, ㅑ, ㅒ, ㅣ
     vowels_1_index = [20, 21, 22, 23, 40]
     vowels_1_1 = [(j, load_image(os.path.join(input_folder, f'{j}.PNG'))) for j in vowels_1_index]
@@ -62,19 +62,26 @@ def combine_letters3(input_folder, output_folder):
     vowels_3_1 = [(m, load_image(os.path.join(input_folder, f'{m}.PNG'))) for m in vowels_3_index]
     vowels_3 = [(v1_value, rescale_image_width(img, 80, 32)) for
                                         v1_value, img in vowels_3_1]
-    #ㅜ, ㅠ, ㅡ
-    vowels_4_index = [33, 37, 38]
+    #ㅜ, ㅠ
+    vowels_4_index = [33, 37]
     vowels_4_1 = [(n, load_image(os.path.join(input_folder, f'{n}.PNG'))) for n in vowels_4_index]
     vowels_4 = [(v1_value, rescale_image_width(img, 80, 32)) for
                                         v1_value, img in vowels_4_1]
     #ㅘ, ㅙ, ㅚ
     vowels_5_index = [29, 30, 31]
     vowels_5 = [(o, load_image(os.path.join(input_folder, f'{o}.PNG'))) for o in vowels_5_index]
-    rescaled_vowels_5 = [(m, rescale_image_width(img, 85, 50)) for m, img in vowels_5]
+    rescaled_vowels_5 = [(m, rescale_image_width(img, 80, 65)) for m, img in vowels_5]
+
     #ㅝ, ㅞ, ㅟ, ㅢ
     vowels_6_index = [34, 35, 36, 39]
     vowels_6 = [(o, load_image(os.path.join(input_folder, f'{o}.PNG'))) for o in vowels_6_index]
-    rescaled_vowels_6 = [(m, rescale_image_ratio(img, rescale_width=80)) for m, img in vowels_6]
+    rescaled_vowels_6 = [(m, rescale_image_width(img, 80, 65)) for m, img in vowels_6]
+
+    #ㅡ
+    vowels_7_index = [38]
+    vowels_7_1 = [(o, load_image(os.path.join(input_folder, f'{o}.PNG'))) for o in vowels_7_index]
+    vowels_7 = [(m, rescale_image_ratio(img, rescale_width=80)) for m, img in vowels_7_1]
+
     final_consonants_index = [1, 3, 6, 8, 10]
     final_consonants = [(l,load_image(os.path.join(input_folder, f'{l}.PNG'))) for l in final_consonants_index]
     rescaled_final_consonants = [(m, rescale_image_width(img, 40, 35)) for m, img in final_consonants]
@@ -107,10 +114,10 @@ def combine_letters3(input_folder, output_folder):
                         continue
                     background = Image.open(background_file)
 
-                    consonant_pos = (20, 10)
-                    vowel_pos = (85, 5)
+                    consonant_pos = (23, 25)
+                    vowel_pos = (85, 20)
                     final_consonant_pos = (37, 83)
-                    second_final_consonant_pos = (72 + second_final_consonant_gap, 87)
+                    second_final_consonant_pos = (70 + second_final_consonant_gap, 85)
 
                     background.paste(consonant, consonant_pos, consonant)
                     background.paste(vowel, vowel_pos, vowel)
@@ -142,10 +149,10 @@ def combine_letters3(input_folder, output_folder):
                         continue
                     background = Image.open(background_file)
 
-                    consonant_pos = (20, 10)
-                    vowel_pos = (80, 5)
-                    final_consonant_pos = (37, 85)
-                    second_final_consonant_pos = (77 + second_final_consonant_gap, 80)
+                    consonant_pos = (33, 25)
+                    vowel_pos = (82, 20)
+                    final_consonant_pos = (43, 83)
+                    second_final_consonant_pos = (75 + second_final_consonant_gap, 85)
 
                     background.paste(consonant, consonant_pos, consonant)
                     background.paste(vowel, vowel_pos, vowel)
@@ -176,8 +183,8 @@ def combine_letters3(input_folder, output_folder):
 
                     background = Image.open(background_file)
 
-                    consonant_pos = (40, 5)
-                    vowel_pos = (45, 35)
+                    consonant_pos = (43, 10)
+                    vowel_pos = (45, 30)
                     final_consonant_pos = (45, 85)
                     second_final_consonant_pos = (75 + second_final_consonant_gap, 85)
 
@@ -212,7 +219,7 @@ def combine_letters3(input_folder, output_folder):
 
                     background = Image.open(background_file)
 
-                    consonant_pos = (40, 10)
+                    consonant_pos = (45, 10)
                     vowel_pos = (45, 38)
                     final_consonant_pos = (53, 95)
                     second_final_consonant_pos = (83 + second_final_consonant_gap, 95)
@@ -248,7 +255,7 @@ def combine_letters3(input_folder, output_folder):
 
                     background = Image.open(background_file)
 
-                    consonant_pos = (50, 0)
+                    consonant_pos = (53, 8)
                     vowel_pos = (40, 60)
                     final_consonant_pos = (43, 95)
                     second_final_consonant_pos = (77 + second_final_consonant_gap, 98)
@@ -284,8 +291,43 @@ def combine_letters3(input_folder, output_folder):
 
                     background = Image.open(background_file)
 
-                    consonant_pos = (50, 15)
+                    consonant_pos = (50, 8)
                     vowel_pos = (40, 67)
+                    final_consonant_pos = (43, 95)
+                    second_final_consonant_pos = (75 + second_final_consonant_gap, 98)
+
+                    background.paste(consonant, consonant_pos, consonant)
+                    background.paste(vowel, vowel_pos, vowel)
+                    background.paste(final_consonant, final_consonant_pos, final_consonant)
+
+                    # second_final_consonant = load_image(os.path.join(input_folder, '10.PNG'))  # 'ㅅ' 더하기
+                    # second_final_consonant = rescale_image_width(second_final_consonant, rescale_width, 18)
+                    background.paste(second_final_consonant, second_final_consonant_pos, second_final_consonant)
+
+                    # output_filename = f"letter_{c_value}_{v_value}_{fc_value}_{sfc_value}.png"
+                    output_filename = 'letter_' + str(c_value).zfill(2) + "_" + str(v_value).zfill(2) + "_" + str(
+                        fc_value).zfill(2) + "_" + str(sfc_value).zfill(2) + ".PNG"
+                    output_path = os.path.join(output_folder, output_filename)
+
+                    background.save(output_path)
+
+            for v, (v_value, vowel) in enumerate(vowels_7):
+                for fc, (fc_value, final_consonant) in enumerate(rescaled_final_consonants):
+                    if fc_value == 1 and sfc_value not in [1, 10]:
+                        continue
+                    elif fc_value == 3 and sfc_value not in [13, 19]:
+                        continue
+                    elif fc_value == 6 and sfc_value not in [1,7,8,10,17,18,19]:
+                        continue
+                    elif fc_value == 8 and sfc_value not in [10]:
+                        continue
+                    elif fc_value == 10 and sfc_value not in [10]:
+                        continue
+
+                    background = Image.open(background_file)
+
+                    consonant_pos = (50, 8)
+                    vowel_pos = (40, 75)
                     final_consonant_pos = (43, 95)
                     second_final_consonant_pos = (75 + second_final_consonant_gap, 98)
 
